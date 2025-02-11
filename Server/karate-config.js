445b4c32c375a0df3157a1a253bd4d349f3fa090
Narendran <narendranmurali2001@gmail.com>
2025-02-11 18:30:11 +0530

    karate to java

--- Server/karate-config.js
+++
@@ -1,7 +0,0 @@
-function fn() {
-    var config = { myprop: 'myvalue', myurl: 'somevalue' };
-    // Remove the callSingle below - it is not needed for configuration.
-    // var result = karate.callSingle('classpath:karate-tests/karate.feature', config);
-    return config;
-  }
-  
